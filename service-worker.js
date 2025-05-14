27d90e484aa824f1f246cbb04857ea1ef5d94c8b
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2025-05-14 22:32:36 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'zenbreak-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/choix-duree.html',
+  '/choix-son.html',
+  '/style.css',
+  '/logo.png'
+];
+
+// Installation du service worker et mise en cache des ressources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activation du service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Récupération des ressources du cache en mode hors ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
