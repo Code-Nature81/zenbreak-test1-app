20cf72831625255ffcab0cd2843ea7b8e553221c
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2025-05-15 21:14:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,24 @@
 const CACHE_NAME = 'zenbreak-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/choix-duree.html',
-  '/choix-son.html',
-  '/style.css',
-  '/logo.png'
+  'index.html',
+  'choix-duree.html',
+  'choix-son.html',
+  'style.css',
+  'logo.png',
+  'sounds/pluie.mp3',
+  'sounds/notification.mp3'
 ];
 
-// Installation du service worker et mise en cache des ressources
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activation du service worker
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Récupération des ressources du cache en mode hors ligne
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
